9042aaeae5319b0cf4e1b48d802277c5f861e035
robweber <robweberjr@gmail.com>
2023-08-27 15:12:04 -0500

    clear out directory before generating images

--- generator.py
+++ generator.py
@@ -1,5 +1,7 @@
 # Generates QR codes from a list
 import argparse
+import glob
+import os
 import os.path
 import qrcode
 import urllib.parse
@@ -15,6 +17,12 @@ def create_qr_code(base_url, title):
     save_path = os.path.join('images', f"{title}.png")
     img.save(save_path)
 
+def purge_directory():
+    files = glob.glob('images/*.png')
+
+    for f in files:
+        os.remove(f)
+
 # parse in the arguments
 parser = argparse.ArgumentParser(description='QR Code Generator')
 parser.add_argument('-i', '--input', required=True, type=argparse.FileType('r'))
@@ -22,9 +30,14 @@ parser.add_argument('-u', '--base_url', required=True, type=str)
 
 args = parser.parse_args()
 
+# clear out the images directory
+purge_directory()
+
 # read in the titles
 titles = args.input.readlines()
 
 # for each title generate the qr code image
+print(f"Generating QR Codes for {len(titles)} titles")
 for t in titles:
     create_qr_code(args.base_url, t.replace('\n', ''))
+print("QR Code genreation complete")
