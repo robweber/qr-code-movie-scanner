9adf4c435737985c707c98bed10256be95442478
robweber <robweberjr@gmail.com>
2023-08-27 15:07:38 -0500

    generate codes based on a text list of titles

--- generator.py
+++ generator.py
@@ -1,11 +1,30 @@
 # Generates QR codes from a list
 import argparse
+import os.path
 import qrcode
+import urllib.parse
 
-website = "https://www.google.com"
+# generates a qr code for the given title
+def create_qr_code(base_url, title):
 
-qr = qrcode.QRCode(version=3)
-qr.add_data(website)
-qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
-img.save('qr.png')
+    qr = qrcode.QRCode(version=3)
+    qr.add_data(f"{base_url}?title={ urllib.parse.quote_plus(title) }")
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+
+    save_path = os.path.join('images', f"{title}.png")
+    img.save(save_path)
+
+# parse in the arguments
+parser = argparse.ArgumentParser(description='QR Code Generator')
+parser.add_argument('-i', '--input', required=True, type=argparse.FileType('r'))
+parser.add_argument('-u', '--base_url', required=True, type=str)
+
+args = parser.parse_args()
+
+# read in the titles
+titles = args.input.readlines()
+
+# for each title generate the qr code image
+for t in titles:
+    create_qr_code(args.base_url, t.replace('\n', ''))
