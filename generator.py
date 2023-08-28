dc1c574b69b6ab5d7ae7f12af847066bf9a5e61c
robweber <robweberjr@gmail.com>
2023-08-27 19:12:10 -0500

    check if title exists in kodi

--- generator.py
+++ generator.py
@@ -5,6 +5,7 @@ import os
 import os.path
 import qrcode
 import urllib.parse
+from kodijson import Kodi
 
 # generates a qr code for the given title
 def create_qr_code(base_url, title):
@@ -23,15 +24,24 @@ def purge_directory():
     for f in files:
         os.remove(f)
 
+def check_exists(title):
+    # try and find this movie in the database
+    result = kodi.VideoLibrary.GetMovies({"filter":{"operator": "is", "field": "title", "value": title}})
+
+    return result['result']['limits']['total']
+
 # parse in the arguments
 parser = argparse.ArgumentParser(description='QR Code Generator')
 parser.add_argument('-i', '--input', required=True, type=argparse.FileType('r'))
 parser.add_argument('-u', '--base_url', required=True, type=str)
-
+parser.add_argument('-k', '--kodi', required=True, type=str)
 args = parser.parse_args()
 
 # clear out the images directory
 purge_directory()
+
+# create Kodi communicator
+kodi = Kodi(f"http://{args.kodi}/jsonrpc")
 
 # read in the titles
 titles = args.input.readlines()
@@ -39,5 +49,10 @@ titles = args.input.readlines()
 # for each title generate the qr code image
 print(f"Generating QR Codes for {len(titles)} titles")
 for t in titles:
-    create_qr_code(args.base_url, t.replace('\n', ''))
+    t_fixed = t.replace('\n', '')
+    if(check_exists(t_fixed)):
+        create_qr_code(args.base_url, t_fixed)
+    else:
+        print(f"{t_fixed} does not exist in Kodi DB")
+
 print("QR Code genreation complete")
