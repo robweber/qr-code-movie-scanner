1f54b3c82f5f431f2609e21a413ea488b90e5a31
Rob Weber <robweberjr@gmail.com>
2023-08-30 13:09:20 -0500

    added options to control qr code layout and size

--- web/codes.php
+++ web/codes.php
@@ -6,26 +6,25 @@
     <?php
       include('config.php');
       $col = 0;
-      $max_col = 3;
       // get all titles from textarea
       $all_titles = $_POST['selected_titles'];
     ?>
-    <table width="100%">
+    <table width="100%" cellPadding="2">
       <?php foreach($all_titles as $f): ?>
       <?php if($col == 0): ?>
-      <tr valign="center">
+      <tr valign="top">
       <?php endif ?>
         <?php $f_split = explode("|", trim($f)) ?>
-        <td width="33%" align="center">
+        <td width="<?php echo 100/$TOTAL_COLS ?>%" align="center">
           <?php $hash = hash("sha256", $SECURITY_CODE . trim($f_split[0])); ?>
-          <img src="qrcode.php?s=qr&t=<?php echo trim($f_split[0]) ?>&d=<?php echo $QR_BASE_URL ?>&c=<?php echo $hash ?>&y=<?php echo $f_spli[1] ?>" />
+          <img src="qrcode.php?s=qr&t=<?php echo trim($f_split[0]) ?>&d=<?php echo $QR_BASE_URL ?>&c=<?php echo $hash ?>&y=<?php echo $f_split[1] ?>" width="<?php echo $QR_SIZE ?>px" height="<?php echo $QR_SIZE ?>px"/>
           <h3><?php echo trim($f_split[0]) ?> (<?php echo $f_split[1] ?>)</h3>
           <?php if($DEBUG_MODE): ?>
           <p><a href="<?php echo $QR_BASE_URL ?>?security=<?php echo $hash ?>&title=<?php echo trim($f_split[0]) ?>&year=<?php echo $f_split[1]?>">QR LINK</a></p>
           <?php endif ?>
         </td>
         <?php $col ++ ?>
-      <?php if($col == 3): ?>
+      <?php if($col == $TOTAL_COLS): ?>
       </tr>
       <?php $col = 0; ?>
       <?php endif ?>
